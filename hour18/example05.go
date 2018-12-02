2ed4e5b3e55fada3ab0613ac5fa93d34f5ecb32f
omer.f.shahul <omer.f.shahul@gmail.com>
2018-12-01 19:16:15 -0500

    commit

---
+++ hour18/example05.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+ "net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+
+   if r.URL.Path != "/" {
+     http.NotFound(w,r)
+     return
+   }
+
+   switch r.Method {
+     case "GET":
+         w.Write([]byte("Received a GET Request\n"))
+     case  "POST":
+         w.Write([]byte("Received a POST Request\n"))
+     default:
+         w.WriteHeader(http.StatusNotImplemented)
+         w.Write([]byte(http.StatusText(http.StatusNotImplemented) + "\n"))
+    }
+}
+
+func main() {
+    http.HandleFunc("/", helloWorld)
+    http.ListenAndServe(":8000", nil)
+}
+
+
