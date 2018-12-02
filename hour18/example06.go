a4328be6c829fa12ff8bdb5eff9425f3f631c2ee
omer.f.shahul <omer.f.shahul@gmail.com>
2018-12-01 20:03:43 -0500

    added example06.go

--- hour18/example06.go
+++ hour18/example06.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func helloWrold(w http.ResponseWriter, r *http.Request) {
+func helloWorld(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
