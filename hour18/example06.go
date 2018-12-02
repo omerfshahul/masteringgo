9ba2f21518060314dcb1ede739e039c2633bfc72
omer.f.shahul <omer.f.shahul@gmail.com>
2018-12-01 19:57:29 -0500

    added example06.go under hour18

---
+++ hour18/example06.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func helloWrold(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(r, w)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		for k, v := range r.URL.Query() {
+			fmt.Printf("%s : %s\n", k, v)
+		}
+	case "POST":
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+
+		}
+		fmt.Printf("%s\n", reqBody)
+		w.Write([]byte("Received a POST"))
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	}
+}
